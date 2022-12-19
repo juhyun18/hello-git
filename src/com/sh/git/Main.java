9413323cb0703e4cf11e285d5d2716499570cc1c
juhyun18 <wngus_0896@naver.com>
2022-12-19 13:02:14 +0900

    Main#master

--- src/com/sh/git/Main.java
+++ src/com/sh/git/Main.java
@@ -10,6 +10,10 @@ package com.sh.git;
  *
  */
 public class Main {
+	public void master() {
+		
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Hello git");
